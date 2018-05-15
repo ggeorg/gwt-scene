d5320b31b73f88501fc501af5340df0fca5c56e0
ggeorg <georgopoulos.georgios@gmail.com>
2018-05-15 17:03:22 +0300

    Canvas WebGL support.

--- src/gwt/scene/ui/client/Canvas.java
+++ src/gwt/scene/ui/client/Canvas.java
@@ -1,8 +1,51 @@
 package gwt.scene.ui.client;
 
+import com.google.gwt.dom.client.CanvasElement;
+
+import gwt.scene.core.client.collections.JsObject;
+import gwt.scene.ui.client.dom.Context2D;
+import gwt.scene.ui.client.dom.WebGLContext;
+
 public class Canvas extends Composite {
 
 	public Canvas() {
 		initWidget(com.google.gwt.canvas.client.Canvas.createIfSupported());
 	}
+
+	@Override
+	protected final Canvas getWidget() {
+		return (Canvas) super.getWidget();
+	}
+
+	public Context2D getContext2d() {
+		return getWidget().getContext2d();
+	}
+
+	/**
+	 * Creates a WebGL 1.0 context upon this canvas.
+	 * 
+	 * @return The created WebGL context object.
+	 */
+	public WebGLContext getContext3d() {
+		return Canvas.getContext3d(getElement().cast(), WebGLContext.CONTEXT_ID, null);
+	}
+
+	private static native WebGLContext getContext3d(CanvasElement canvas, String[] contextType,
+			JsObject attributes) /*-{
+		var gl = null;
+		for (var i = 0; i < contextType.length; ++i) {
+			try {
+				if (attributes) {
+					gl = canvas.getContext(contextType[i], attributes);
+				} else {
+					gl = canvas.getContext(contextType[i]);
+				}
+			} catch (e) {
+			}
+			if (gl) {
+				break;
+			}
+		}
+		return gl;
+	}-*/;
 }
